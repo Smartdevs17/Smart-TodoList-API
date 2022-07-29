da9cc541a44bf1767143369f7e88940e22ab52ff
Smart Developer <smartdevs17@gmail.com>
2022-07-29 10:04:32 +0100

    add testdb

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+let env = process.env.NODE_ENV || "development";
+
+if(env === "development"){
+    process.env.PORT = 5000;
+    process.env.MONGO_URL = "mongodb://localhost:27017/smartTodoDB"
+}else if(env === "test"){
+    process.env.PORT = 3000;
+    process.env.MONGO_URL = "mongodb://localhost:27017/smartTodoDBTest"
+};
+
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -17,7 +27,7 @@ app.use("/api/todos",todoRoute);
 
 
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 app.listen(port,() => {
     console.log(`Server stated running on port ${port}`);
 });
--- db/config-db.js
+++ db/config-db.js
@@ -1,7 +1,7 @@
 let mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
-mongoose.connect("mongodb://localhost:27017/smartTodoDB",{useNewUrlParser: true});
+mongoose.connect(process.env.MONGO_URL,{useNewUrlParser: true});
 const connection = mongoose.connection;
 connection.on("open",() => console.log("Connected to DB"));
 connection.once("error",(error) => console.error(error.message));
