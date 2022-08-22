a3f11d0520beddd7a8c72c169401e9c2a5ff14bc
Smart Developer <smartdevs17@gmail.com>
2022-08-22 09:54:46 +0100

    add mongodb atlas

--- db/config-db.js
+++ db/config-db.js
@@ -1,7 +1,7 @@
 let mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGO_URL,{useNewUrlParser: true});
+mongoose.connect(process.env.MOONGODB_URI || process.env.MONGO_URL,{useNewUrlParser: true});
 const connection = mongoose.connection;
 connection.on("open",() => console.log("Connected to DB"));
 connection.once("error",(error) => console.error(error.message));
